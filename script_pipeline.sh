8a2222fa61c3fcce5f0ccda38c2a037edb42d120
mobsit <38907296+mobsit@users.noreply.github.com>
2023-01-26 22:52:00 +0100

    Update script_pipeline.sh

--- script_pipeline.sh
+++ script_pipeline.sh
@@ -1,19 +1,19 @@
 
-#!/bin/sh
-p=$(git log  --pretty=oneline   --reverse | sed 's/\|/ /'|awk '{print $1}' | awk -v string="$1" -f tst.awk)
-# my_array=( $(git log --pretty=oneline --reverse | sed 's/\|/ /'|awk '{print $1}') )
-my_array=( $(git log --name-status "$1"^.."$1" | sed -e '1,6d'|awk '{print $2}') )
-# if [ "$p" = 1 ]
-#   then 
-#       mkdir data/N
-#       cp --parents readme.md data/N
-#   else
-      
-length=${#my_array[@]}
-echo $length
-echo "$p"
-mkdir  data/N+"$p" 
-for (( j=0; j<${length} ; j++ ));
-    do
-        cp --parents "${my_array[j]}" data/N+"$p"
-    done
+#!/bin/bash
+
+echo "******* script started *****"
+echo "*****************"
+
+# Track changes to the master branch
+git checkout master
+
+# Save the SHA-1 hash of the latest commit
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+# Create a directory for the latest commit
+mkdir -p data/$LATEST_COMMIT
+
+# Copy all modified files from the latest commit to the new directory
+for file in $(git diff-tree --no-commit-id --name-only -r $LATEST_COMMIT); do
+    cp $file data/$LATEST_COMMIT
+done
